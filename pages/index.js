1bca926a431d04b6a468a649f305da2025222254
saku-g <sakucode1993@gmail.com>
2024-02-16 09:32:48 +0900

    Add Chapter4 code

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import Container from '@/components/container';
 import Hero from '@/components/hero';
 
 export default function Home() {
   const subtitle = 'アウトプットしていくサイト';
 
-  return <Hero title="CUBE" subtitle="アウトプットしていくサイト" imageOn />;
+  return (
+    <Container>
+      <Hero title="CUBE" subtitle="アウトプットしていくサイト" imageOn />
+    </Container>
+  );
 }

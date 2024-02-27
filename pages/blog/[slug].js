a1a367af6865facdd81298629c4a40106709548e
saku-g <sakucode1993@gmail.com>
2024-02-27 21:20:34 +0900

    Add chapter9 code

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,5 +1,6 @@
-import { getPostBySlug } from '@/lib/api';
+import { getPostBySlug, getAllSlugs } from '@/lib/api';
 import { extractText } from '@/lib/extract-text';
+import { prevNextPost } from '@/lib/prev-next-post';
 import Meta from '@/components/meta';
 import Container from '@/components/container';
 import PostHeader from '@/components/post-header';
@@ -7,9 +8,14 @@ import PostBody from '@/components/post-body';
 import { TwoColumn, TwoColumnMain, TwoColumnSidebar } from '@/components/two-column';
 import ConvertBody from '@/components/convert-body';
 import PostCategories from '@/components/post-categories';
+import Pagination from '@/components/pagination';
 import Image from 'next/image';
+import { getPlaiceholder } from 'plaiceholder';
 
-export default function Schedule({ title, publish, content, eyecatch, categories, description }) {
+// ローカルの代替アイキャッチ画像
+import { eyecatchLocal } from '@/lib/constants';
+
+export default function Post({ title, publish, content, eyecatch, categories, description, prevPost, nextPost }) {
   return (
     <Container>
       <Meta
@@ -24,12 +30,15 @@ export default function Schedule({ title, publish, content, eyecatch, categories
 
         <figure>
           <Image
+            key={eyecatch.url}
             src={eyecatch.url}
             alt=""
             width={1152}
             height={576}
             sizes="(min-width: 1152px) 1152px, 100vw"
             priority
+            placeholder="blur"
+            blurDataURL={eyecatch.blurDataURL}
           />
         </figure>
 
@@ -43,27 +52,51 @@ export default function Schedule({ title, publish, content, eyecatch, categories
             <PostCategories categories={categories} />
           </TwoColumnSidebar>
         </TwoColumn>
+
+        <Pagination
+          prevText={prevPost.title}
+          prevUrl={`/blog/${prevPost.slug}`}
+          nextText={nextPost.title}
+          nextUrl={`/blog/${nextPost.slug}`}
+        />
       </article>
     </Container>
   );
 }
 
-export async function getStaticProps() {
-  const slug = 'schedule';
+export async function getStaticPaths() {
+  const allSlugs = await getAllSlugs();
+
+  return {
+    paths: allSlugs.map(({ slug }) => `/blog/${slug}`),
+    fallback: 'blocking',
+  };
+}
+
+export async function getStaticProps(context) {
+  const slug = context.params.slug;
 
   const post = await getPostBySlug(slug);
-  // console.log(post);
 
   const description = extractText(post.content);
+  const eyecatch = post.eyecatch ?? eyecatchLocal;
+
+  const { base64 } = await getPlaiceholder(eyecatch.url);
+  eyecatch.blurDataURL = base64;
+
+  const allSlugs = await getAllSlugs();
+  const [prevPost, nextPost] = prevNextPost(allSlugs, slug);
 
   return {
     props: {
       title: post.title,
       publish: post.publishDate,
       content: post.content,
-      eyecatch: post.eyecatch,
+      eyecatch: eyecatch,
       categories: post.categories,
       description: description,
+      prevPost: prevPost,
+      nextPost: nextPost,
     },
   };
 }
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,39 @@
+import { getAllPosts } from '@/lib/api';
 import Meta from '@/components/meta';
 import Container from '@/components/container';
 import Hero from '@/components/hero';
+import Posts from '@/components/posts';
+import Pagination from '@/components/pagination';
+import { getPlaiceholder } from 'plaiceholder';
 
-export default function Home() {
-  const subtitle = 'アウトプットしていくサイト';
+// ローカルの代替アイキャッチ画像
+import { eyecatchLocal } from '@/lib/constants';
 
+export default function Home({ posts }) {
   return (
     <Container>
       <Meta />
       <Hero title="CUBE" subtitle="アウトプットしていくサイト" imageOn />
+      <Posts posts={posts} />
     </Container>
   );
+}
+
+export async function getStaticProps() {
+  const posts = await getAllPosts(4);
+
+  for (const post of posts) {
+    if (!post.hasOwnProperty('eyecatch')) {
+      post.eyecatch = eyecatchLocal;
+    }
+
+    const { base64 } = await getPlaiceholder(post.eyecatch.url);
+    post.eyecatch.blurDataURL = base64;
+  }
+
+  return {
+    props: {
+      posts: posts,
+    },
+  };
 }
